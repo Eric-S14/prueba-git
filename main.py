e237c2c1dbadf1703d11bcb72b03982de6d4f399
Eric-S14 <ericsarwar1@gmail.com>
2024-12-29 13:29:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-main.py
+def redondear(numero):
+    if numero > 3.50:
+        return int(numero) + 1
+    else:
+        return int(numero)
+
+def suma_redondeada(a, b):
+    resultado = a + b
+    return redondear(resultado)
+
+numero1 = 2.45
+numero2 = 1.10
+print(f"La suma redondeada de {numero1} y {numero2} es: {suma_redondeada(numero1, numero2)}")
